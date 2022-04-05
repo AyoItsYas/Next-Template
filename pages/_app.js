4b3a0d67bee8e33786e55ebaf6c3bd8a3695b227
ItsYasiru <mwmykd@gmail.com>
2022-04-05 19:02:58 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/_app.sass';
+
+
+function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import classes from '../styles/index.module.sass'
+
+
+function IndexPage() {
+    return (
+        <div className={classes.main}>
+            <h1>Index</h1>
+        </div>
+    );
+};
+
+
+export default IndexPage;
